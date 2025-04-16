aa212829c6a3a6cb3f14686c9387cc1bdc7f3952
Difreyma <difreyma@unal.edu.co>
2025-04-16 17:44:14 -0500

    login

--- frontend/src/app/componentes/login/login.component.ts
+++ frontend/src/app/componentes/login/login.component.ts
@@ -2,13 +2,23 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 
+
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [FooterComponent, HeaderComponent],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  isRegisterActive: boolean = false;
+
+  showRegister(): void {
+    this.isRegisterActive = true;
+  }
+
+  showLogin(): void {
+    this.isRegisterActive = false;
+  }
 
 }
