f351121b3e612052c96602fb173042826a186dea
ManuelMorgan456 <morgangithub@gmail.com>
2025-04-16 17:47:56 -0500

    Se abrio nueva rama

--- frontend/src/app/componentes/login/login.component.ts
+++ frontend/src/app/componentes/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 
+
 @Component({
   selector: 'app-login',
   standalone: true,
