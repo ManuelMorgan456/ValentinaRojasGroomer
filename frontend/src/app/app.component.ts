8dd6298d55749828b3d824c865c35844d9cc234b
ManuelMorgan456 <morgangithub@gmail.com>
2025-04-19 22:41:55 -0500

    Se corrige app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './componentes/header/header.component';
-import { FooterComponent } from './componentes/footer/footer.component';
+// import { HeaderComponent } from './componentes/header/header.component';
+// import { FooterComponent } from './componentes/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,7 +8,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet]
+  imports: [RouterOutlet] //HeaderComponent, FooterComponent, 
 })
 export class AppComponent {
   title = 'frontend';
