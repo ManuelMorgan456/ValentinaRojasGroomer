e8bee23808d45bbd0d95bb81248eaa4f9014088e
ManuelMorgan456 <morgangithub@gmail.com>
2025-04-21 01:18:20 -0500

    Se crea el footer final para desktop

--- frontend/src/app/componentes/footer/footer.component.ts
+++ frontend/src/app/componentes/footer/footer.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-footer',
   standalone: true,
+
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'] // corregido
 })
